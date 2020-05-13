5d2292fd3af85ac34ced2361537d68809f5b02bf
ReZero <ljrezero@gmail.com>
2020-05-13 22:53:09 +0800

    Update util.js

--- src/main/resources/js/util.js
+++ src/main/resources/js/util.js
@@ -1,10 +1,12 @@
-function jsonToQueryString(params) {
-    let paramsArray = Object.keys(params).filter((key, _) => {
-      if (params.hasOwnProperty(key)) {
-        let val = params[key];
-        return !(val === null || typeof val === 'undefined')
-      }
-    }).map((key, _) => (key + '=' + params[key]));
-    return queryString = paramsArray.length > 0 ? "?" + paramsArray.join("&") : "";
-}
-    
+jsonToQueryString(params) {
+    const paramsArray = Object.keys(params)
+      .filter(
+        (key) => {
+          if (Object.prototype.hasOwnProperty.call(params, key)) {
+            const val = params[key];
+            return !(val === null || typeof val === 'undefined');
+          }
+          return false;
+    }).map((key) => (`${key}=${params[key]}`));
+    return paramsArray.length > 0 ? `?${  paramsArray.join("&")}` : "";
+},
