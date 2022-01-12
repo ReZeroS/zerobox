2a97298ea1bdd54e3a287e83770ae31667142b11
lijie78 <lijie78@58.com>
2022-01-12 19:07:14 +0800

    add autoclick.js

---
+++ src/main/resources/expression/autoclick.js
@@ -0,0 +1,4 @@
+// right click and use copy selector
+var autoClick=setInterval(function(){
+    document.querySelector("#okr-content-list-wrapper > div.OKR-okr-list-wrapper-2s2IY > div > div.hidden-with-print.ObjectiveCreator-objective-creator-10bX3 > span").click();//.clock-tip p是弹窗按钮的HTML代码，.clock-tip是一个命名为clock-tip的类，如果是ID就用#，有一定的HTML知识很好理解
+},6*100);
